6ca2c2661ea0fdc642d5077a72b577270a17837d
Jose A Tula Leyva <joseph.tula@gmail.com>
2017-06-15 09:46:47 -0300

    demo

---
+++ Assets/buttonScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class buttonScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+    private GameObject vbButtonObject;
+    private GameObject butterflyObject;
+
+    // Use this for initialization
+    void Start () {
+        vbButtonObject = GameObject.Find("actionButton");
+        butterflyObject = GameObject.Find("Butterfly");
+        vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+	}
+
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("botton Pressed");
+        butterflyObject.GetComponent<Animation>().Play();
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+
+
+        butterflyObject.GetComponent<Animation>().Stop();
+        Debug.Log("button release");        
+
+    }
+
+}
